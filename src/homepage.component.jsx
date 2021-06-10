bd6165f9c5843b410da13e24b2141b68d988d5b2
Mrekyle <64297454+Mrekyle@users.noreply.github.com>
2021-06-10 13:26:36 -0700

    First Lesson - V0.0.1

---
+++ src/homepage.component.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage =() => (
+    <div class='homepage'>
+        <div class='directory-menu'>
+            <div class='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>Shop Now!</span>
+                </div>
+            </div>
+            <div class='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>JACKETS</h1>
+                        <span className='subtitle'>Shop Now!</span>
+                </div>
+            </div>
+            <div class='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>WOMENS</h1>
+                        <span className='subtitle'>Shop Now!</span>
+                </div>
+            </div>
+            <div class='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>MENS</h1>
+                        <span className='subtitle'>Shop Now!</span>
+                </div>
+            </div>
+            <div class='menu-item'>
+                <div class='content'>
+                    <h1 className='title'>SHOES</h1>
+                        <span className='subtitle'>Shop Now!</span>
+                </div>
+            </div>
+        </div>
+    </div>
+) 
+
+export default HomePage;
